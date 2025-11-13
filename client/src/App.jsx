21a19924f631b2cd2e19d0ad08dd657dd66c8a4d
Prajyot <bprajyot2234@gmail.com>
2025-11-13 21:14:52 +0530

    final app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Dashboard from './pages/dashboard/Dashboard'
 import Navbar from './components/Navbar'
+import Upload from './pages/upload/Upload';
+import DocumentDetails from './pages/reports/DocumentDetails';
+import Documents from './pages/reports/Documents';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/documents" element={<Documents />} />
+      <Route path="/documents/:id" element={<DocumentDetails />} />
     </Routes>
   )
 }
